0ca2f365a010c6c33b3f8bf552f20244bfb954dc
Holger Zerbe <holger.zerbe@web.de>
2021-05-17 23:07:04 +0200

    new routing

---
+++ code/app/build/precache-manifest.0534a87415c0ee2f26dc16071a4621c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "591aaf11477fb5f0ac6e7d302ac58b88",
+    "url": "/haker_newDesign/index.html"
+  },
+  {
+    "revision": "b97ea8144813962ddf93",
+    "url": "/haker_newDesign/static/css/main.46031947.chunk.css"
+  },
+  {
+    "revision": "9ebd69f90a14697124bf",
+    "url": "/haker_newDesign/static/js/2.273b7184.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/haker_newDesign/static/js/2.273b7184.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b97ea8144813962ddf93",
+    "url": "/haker_newDesign/static/js/main.f6d7c3dc.chunk.js"
+  },
+  {
+    "revision": "078e600ebb2a9f76998a",
+    "url": "/haker_newDesign/static/js/runtime-main.4c777364.js"
+  }
+]);
