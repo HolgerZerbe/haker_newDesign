c48734c4ee8b34e4766471b741707f7d693b3523
Holger Zerbe <holger.zerbe@web.de>
2022-11-13 13:43:36 +0100

    Updates

---
+++ precache-manifest.5a6ec069f260e8ad7a01827ee8d86055.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10e1edc866aecd2f6a522341e6a8c843",
+    "url": "/haker_newDesign/index.html"
+  },
+  {
+    "revision": "2b584ecc888b561992c8",
+    "url": "/haker_newDesign/static/css/main.42b8016f.chunk.css"
+  },
+  {
+    "revision": "788c8e55ffe417ed0a51",
+    "url": "/haker_newDesign/static/js/2.71137243.chunk.js"
+  },
+  {
+    "revision": "cdb02cec5f135950b37e7b1380fc3772",
+    "url": "/haker_newDesign/static/js/2.71137243.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b584ecc888b561992c8",
+    "url": "/haker_newDesign/static/js/main.83aab85f.chunk.js"
+  },
+  {
+    "revision": "2285bac04bc8cff72dee",
+    "url": "/haker_newDesign/static/js/runtime-main.4afb05ff.js"
+  }
+]);
